27a0b64fa2cc02d96d87441d0b7fd214ca64f6be
Phan Gia Thai <thaigp@paxcreation.com>
2021-02-23 14:51:22 +0700

    init

---
+++ Guitar/Guitar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guitar
+//
+//  Created by paxcreation on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("kookkkoko")
+    }
+
+
+}
+
